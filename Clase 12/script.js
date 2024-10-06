28e37492a0f12909a4717e5a4a72f28c5e65b458
AgustinSteinberg <leonelsteinberg@gmail.com>
2024-10-06 11:42:57 -0300

    JS Introducción

---
+++ Clase 12/script.js
@@ -0,0 +1,79 @@
+//Comentario unilínea
+/* Comentario multilínea */
+
+/* TIPOS DE DATOS */
+
+/* PRIMITIVOS */
+
+/* 
+STRINGS: Se escriben entre comillas('',"",``) y representan palabras alfanuméricas o textos.
+BOOLEANOS/BOOLEAN: true o false. Se usa para representar estados booleanos o binarios.
+NUMBERS: Numeros.
+
+NUMEROS ESPECIALES
+NaN:Not a number. Es cuando se intenta pasar otro dato a numérico y no se puede. Es un tipo de dato numérico.
+Infinity: Cualquier resultado que tiende a infinito. Es un tipo de dato numérico.
+
+NULOS/NULL:Representa ausencia de dato/No hay respuesta
+
+UNDEFINED: Representa un dato sin definir.
+ */
+
+/* 
+'Pepe'
+"Pepe"
+`Pepe` 
+*/
+
+/* Constructores/Prototipos*/
+/* Nos permiten pasar de un dato a otro dato.
+
+Number: Convierte un dato en number
+Boolean: Convierte un dato en Boolean
+String: Convierte un dato en String
+
+ */ 
+/* 
+Typeof devuelve como string el tipo de dato
+*/
+
+console.log(
+    typeof(Boolean(1))
+)
+
+console.log(
+    typeof(Number(8))
+)
+
+console.log(
+    typeof(null)
+)
+
+/* ERRORES DE NULL */
+
+/* 
+console.log(null==0)
+console.log(null >=0)
+console.log(null<=0)
+console.log(null>0)
+console.log(Number(null)) 
+*/
+
+
+/* Palabra reservada
+Number
+Boolean 
+String
+Type of
+*/
+
+/* Console: Herramienta que nos permite mostrar valores de nuestro programa
+
+*/
+
+/* Console log: Registrar en consola*/
+
+console.log('Hola')
+
+/* OBJETOS */
+
